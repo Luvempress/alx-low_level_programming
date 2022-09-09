de2b0bbdaed50f09e9ca6c5fddc19d5bb613e2f3
Okpala Immaculata <okpalaimmaculata01@gmail.com>
2022-09-08 18:57:49 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
- * main - print the phrase
- * Return: 0 if exited properly
+ * main - print the phrase "Programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, non zero otherwise
  */
 int main(void)
 {
-	puts( "Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }
 
