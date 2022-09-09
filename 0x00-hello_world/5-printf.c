ab40a6764be1bc8e7e1fc5fe0f2e6e4336e3c9d5
Okpala Immaculata Onyinye <110991195+Luvempress@users.noreply.github.com>
2022-09-09 04:35:23 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Prints the phrase
- * "with proper grammer, but the outcome is a piece of art,"
- * RETURN: 0 if exited properly, non-zero otherwise
- **/
-intmain(void)
+ * main - main block, this function should print the sentence
+ * Return: 0
+ */
+int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
