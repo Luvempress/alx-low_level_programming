73509a49df7a086512a979546cd20697202bbdee
Okpala Immaculata <okpalaimmaculata01@gmail.com>
2022-09-08 20:29:35 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-#include <stdio>
+#include <stdio.h>
 /**
- * main - Prints the phrase "with proper grammer, but
- * the outcome is a piece of art,"
+ * main - Prints the phrase
+ * "with proper grammer, but the outcome is a piece of art,"
  * RETURN: 0 if exited properly, non-zero otherwise
  **/
 int main(void)
 {
-	printf("with proper grammer, but the
-			outcome is a piece of art,\n");
+	printf("with proper grammer, but theoutcome is a piece of art,\n");
 	return (0);
 }
