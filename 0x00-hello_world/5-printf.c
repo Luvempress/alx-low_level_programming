f3d49d0ee7b6fc4390d7776c0d82f2604e592328
Okpala Immaculata <okpalaimmaculata01@gmail.com>
2022-09-08 20:21:01 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio>
+/**
+ * main - Prints the phrase "with proper grammer, but
+ * the outcome is a piece of art,"
+ * RETURN: 0 if exited properly, non-zero otherwise
+ **/
+int main(void)
+{
+	printf("with proper grammer, but the
+			outcome is a piece of art,\n");
+	return (0);
+}
